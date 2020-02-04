fbf760c76245255506dc1792ac13e74632c83acd
Bradley <bmcguigandev@outlook.com>
2020-02-04 01:45:36 +0000

    add tagging feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+
         @comment = Comment.new
         @comment.article_id = @article.id
     end
@@ -45,5 +46,4 @@ class ArticlesController < ApplicationController
 
         redirect_to article_path(@article)
     end
-
 end
